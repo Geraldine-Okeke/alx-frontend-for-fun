3d80203483f06573a367cc0109d3d2af91b3b7ed
Geraldine-Okeke <me.ejiofor@unizik.edu.ng>
2023-07-14 12:41:33 +0100

    task 0

---
+++ markdown2html.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Script that converts a Markdown file to HTML.
+"""
+
+import sys
+import os.path
+
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        print("Usage: ./markdown2html.py <input_file> <output_file>", file=sys.stderr)
+        sys.exit(1)
+
+    input_file = sys.argv[1]
+    output_file = sys.argv[2]
+
+    if not os.path.isfile(input_file):
+        print(f"Missing {input_file}", file=sys.stderr)
+        sys.exit(1)
+
+    # Convert Markdown to HTML
+    # Add your code here
+
+    sys.exit(0)
